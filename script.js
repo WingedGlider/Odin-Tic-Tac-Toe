fee919017e54a33c7473c0b4afd5f0782a38d1f9
WingedGlider <william.armstrong967@gmail.com>
2024-01-08 13:16:13 -0500

    in-browser game playable

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const game = function () {
-    const grid = [];
+    let grid = [];
     let playerchoice;
     let computerchoice;
-    let gamestate;
     let winner;
+    let gamestate;
     const selectscreen = document.querySelector('body').innerHTML;
 
     const select = function (){
@@ -18,61 +18,93 @@ const game = function () {
             if(document.querySelector('.active') != null) initialize()
         });
     }
+    
     const initialize = function (){
-        playerchoice = document.querySelector('.active').innerHTML.toLowerCase();
+        gamestate = true;
+        if (document.querySelector('.active') != null) playerchoice = document.querySelector('.active').innerHTML.toLowerCase();
         document.querySelector('body').innerHTML = '';
         document.querySelector('body').innerHTML = '<h1>Tic-Tac-Toe</h1><div class=gameboard></div>';
         for(let i = 0; i < 3; i++){
             document.querySelector('.gameboard').innerHTML += '<div></div>';
-            let array = [] 
+            let array = []; 
             for(let j = 0; j < 3; j++){
                 array.push('');
-                document.querySelector('.gameboard>div:nth-child('+(i+1)+')').innerHTML += '<div class=tile></div>';
+                document.querySelector('.gameboard>div:nth-child('+(i+1)+')').innerHTML += '<div class=tile><p></p></div>';
             }
             grid.push(array);
         }
         document.querySelectorAll('.tile').forEach(tile =>{
             tile.addEventListener('click', ()=>{
-                play();
+                if (tile.querySelector('p').innerHTML == '') play(tile);
             });
         });
-        if (playerchoice = 'x') computerchoice = 'o';
+        if (playerchoice == 'x') computerchoice = 'o';
         else computerchoice = 'x';
     }
 
-    const play = function(){
-        //play should be passed the currently pressed div
-        //inner HTML should be changed to playerchoice.toUpperCase();
-        //listener should be stripped
-        //grid of x/y assigned
-        //winstate checked
-        //if winstate, modal
-        //else run player 2/computer
-        gamestate = true;
-        while(gamestate){
-            let x = prompt('enter first index');
-            let y = prompt('enter second index');
-            grid[x][y] = playerchoice;
-            winstate(x,y);
+    const play = function(currentTile){
+        currentTile.querySelector('p').innerHTML = playerchoice;
+        addTile(currentTile, playerchoice);
+        winstate(currentTile);
+        console.log(gamestate);
+        if (gamestate == false) endScreen();
+        else{ 
+            currentTile = compTurn();
+            currentTile.querySelector('p').innerHTML = computerchoice;
+            addTile(currentTile, computerchoice)
+            winstate(currentTile); 
+            if (gamestate == false) endScreen();
         }
     }
 
-    const winstate = function(x, y){
+    const addTile = function(currentTile, choice){
+        const tileArray = document.querySelectorAll('.tile');
+        const gridArray = [] 
+        grid.forEach(array => array.forEach(cell => gridArray.push(cell)));
+        for(let i = 0; i < tileArray.length; i++){
+            if (tileArray[i] === currentTile) gridArray[i] = choice;
+        }
+        let counter = 0;
+        grid = [];
+        for(let i = 0; i < 3; i++){
+            let array = [];
+            for(let j = 0; j < 3; j++){
+                array.push(gridArray[counter]);
+                counter++;
+            }
+            grid.push(array);
+        }
+    }
+
+    const compTurn = function(){ 
+        const tileArray = document.querySelectorAll('.tile');
+        let randNum = Math.floor((Math.random()*9)+1);
+        currentTile = tileArray[randNum-1];
+        if  (currentTile.querySelector('p').innerHTML != '') currentTile = compTurn();
+        return currentTile;
+    }
+
+    const winstate = function(currentTile){
         //add a tie check
-        if(    (grid[0][0] == state && grid[0][1] == state && grid[0][2] == state) 
-            || (grid[1][0] == state && grid[1][1] == state && grid[1][2] == state) 
-            || (grid[2][0] == state && grid[2][1] == state && grid[2][2] == state) 
-            || (grid[0][0] == state && grid[1][0] == state && grid[2][0] == state) 
-            || (grid[0][1] == state && grid[1][1] == state && grid[2][1] == state) 
-            || (grid[0][2] == state && grid[1][2] == state && grid[2][2] == state) 
-            || (grid[0][0] == state && grid[1][1] == state && grid[2][2] == state) 
-            || (grid[0][2] == state && grid[1][1] == state && grid[2][2] == state)) 
+        if(    (grid[0][0] == playerchoice && grid[0][1] == playerchoice && grid[0][2] == playerchoice)
+            || (grid[1][0] == playerchoice && grid[1][1] == playerchoice && grid[1][2] == playerchoice) 
+            || (grid[2][0] == playerchoice && grid[2][1] == playerchoice && grid[2][2] == playerchoice) 
+            || (grid[0][0] == playerchoice && grid[1][0] == playerchoice && grid[2][0] == playerchoice) 
+            || (grid[0][1] == playerchoice && grid[1][1] == playerchoice && grid[2][1] == playerchoice) 
+            || (grid[0][2] == playerchoice && grid[1][2] == playerchoice && grid[2][2] == playerchoice) 
+            || (grid[0][0] == playerchoice && grid[1][1] == playerchoice && grid[2][2] == playerchoice) 
+            || (grid[0][2] == playerchoice && grid[1][1] == playerchoice && grid[2][0] == playerchoice)) 
             gamestate = false;
-            if (grid[x][y] == playerchoice) winner = 'player';
-            else if(grid[x][y] == computerchoice) winner == 'computer';
+            if (currentTile.querySelector('p').innerHTML == playerchoice) winner = 'You are the winner!';
+            else if(currentTile.querySelector('p').innerHTML == computerchoice) winner == 'The computer took this round...';
         return;
     }
-    return {initialize, select};
+
+    const endScreen = function(){
+        //add the wintext, losetext, or tie text to the screen
+        //+ 2 buttons that decide whether the select screen or game screen is loaded
+    }
+    return {select};
 }();
 
 game.select();
